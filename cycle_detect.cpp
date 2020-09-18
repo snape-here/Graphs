a707e9c93a53f87578858702c7d77895086bf80b
Susanta Mukherjee <susantamukherjee392@gmail.com>
2020-09-18 11:27:43 +0530

    Made changes to cycle_detect for Readability

--- cycle_detect.cpp
+++ cycle_detect.cpp
@@ -10,48 +10,73 @@ using namespace std;
 #define ss second
 #define mod 1000000007
 #define endl "\n"
+#define FIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
-const int MAX=5e18;
+const int N=1e5+5;
 
-const int N=1e6+5;
+vector<int> G[N],Gr[N];
+vector<pi> ve;
 
-const int mx=20;
+int vis[N]={0},post[N];
 
-int n,m;
+int c=1;
 
-vector<int>arr[N];
-
-int vis[N];
-
-bool dfs(int n , int par)
+void dfs(int p)
 {
-    vis[n] = 1;
-    for(int i : arr[n])
-    {
-        if(!vis[i])
-        {
-            if(dfs(i,n))
-                return true;
-        }
-        else
-            if(i!=par)
-                return true;
-    }
-    return false;
+    vis[p]=1;
+
+    for(int child: G[p])
+        if(!vis[child])
+            dfs(child);
+
+    post[p]=c++;
 }
 
 signed main()
 {
-
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
+ 
+    FIO
 
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
     #endif
 
-    
+    int n,m,u,v;
 
+    cin>>n>>m;
+
+    for(int i=0; i<m; i++)
+    {
+        cin>>u>>v;
+        G[u].pb(v);
+        ve.pb({u,v});
+    }
+
+    for(int i=1; i<=n; i++)
+    {
+        if(!vis[i])
+            dfs(i);
+    }
+
+    int flag = 0;
+
+    for(auto it: ve)
+    {
+        int a = it.ff;
+        int b = it.ss;
+        if(post[a]<post[b])
+        {
+            flag=1;
+            break;
+        }
+    }
+
+    if(flag)
+        cout<<"Cycle Present"<<endl;
+    else
+        cout<<"Acyclic"<<endl;
+ 
     return 0;
    
 }
